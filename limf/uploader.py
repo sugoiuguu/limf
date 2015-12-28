35fb35c08e9d076bd7902ef32f9dbdb403a42c84
Mikołaj 'lich' Halber <lich@openmailbox.org>
2015-12-28 13:28:46 +0100

    Added glop support.

--- limf/uploader.py
+++ limf/uploader.py
@@ -16,7 +16,8 @@ def upload_files(selected_file, selected_host, only_link, file_name):
         answer = requests.post(
             url=selected_host[0]+"upload.php",
             files={'files[]':selected_file})
-        file_name_1 = re.findall(r'"url": *"((h.+\/){0,1}(.+))"[,\}]', answer.text)[0][2]
+        file_name_1 = re.findall(r'"url": *"((h.+\/){0,1}(.+))"[,\}]', \
+            answer.text.replace("\\", ""))[0][2]
         if only_link:
             return selected_host[1]+file_name_1
         else:
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='limf',
-    version='0.4.3',
+    version='0.4.4',
     url='http://github.com/lich/limf',
     license='MIT',
     author='Mikolaj \'lich\' Halber',
